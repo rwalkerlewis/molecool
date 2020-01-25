01fc487f5837eda3428b7ae4846123f72e95784c
Robert Walker <rlwalker@usc.edu>
2020-01-25 16:25:28 -0500

    added a few unit tests

--- molecool/tests/test_measure.py
+++ molecool/tests/test_measure.py
@@ -38,3 +38,17 @@ def test_calculate_angle():
     calcuated_measure = molecool.calculate_angle(rA, rB, rC, degrees=True)
     
     assert calcuated_measure == expected_measure
+    
+    
+#@pytest.mark.parametrize("variable_name1, variable_name2, variable_name3, ...., variable_nameN")
+ 
+@pytest.mark.parametrize("p1, p2, p3, expected_angle", [
+    (np.array([np.sqrt(2)/2, np.sqrt(2)/2,  0]), np.array([0, 0, 0]), np.array([1, 0, 0]), 45.0),
+    (np.array([           0,            0, -1]), np.array([0, 1, 0]), np.array([1, 0, 0]), 60.0) ])
+
+
+def test_calculate_angle_many(p1, p2, p3, expected_angle):
+    
+    calculate_angle = molecool.calculate_angle(p1, p2, p3, degrees=True)
+    
+    assert expected_angle == pytest.approx(calculate_angle)
